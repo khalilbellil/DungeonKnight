05d99b88a7c3f41f2f55bef346c1d4f192594343
khalilbellil <khalilbellil.ca@gmail.com>
2019-06-05 10:11:39 -0400

    initial commit

---
+++ Assets/Scripts/Flow/GameFlow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GameFlow : Flow
+{
+    public override void Initialize()
+    {
+
+
+        initialized = true;
+    }
+
+    public override void Update(float dt)
+    {
+
+        Debug.Log("GAMEFLOW");
+    }
+
+    public override void FixedUpdate(float dt)
+    {
+
+    }
+
+    public override void EndFlow()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Flow/MenuFlow.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuFlow : Flow
+{
+    Canvas menuCanvas;
+    Button playButton, optionsButton, exitButton;
+    bool startMenuPassed = false;
+
+    public override void Initialize()
+    {
+
+
+        initialized = true;
+    }
+
+    public override void Update(float dt)
+    {
+        Debug.Log("MENUFLOW");
+    }
+
+    public override void FixedUpdate(float dt)
+    {
+
+    }
+
+    public override void EndFlow()
+    {
+
+    }
+
+    void PlayButton()
+    {
+        Debug.Log("PLAY");
+
+        MainEntry.sceneNb = 2;//Switch to Game Scene
+    }
+
+    void OptionsButton()
+    {
+        Debug.Log("OPTIONS");
+
+    }
+
+    void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("EXIT");
+    }
+
+}
