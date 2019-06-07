9b622deff837999d8c288a3233b4c21ae2f582c7
Philippe Bousquet <pbousque@isimtl.local>
2019-06-07 15:03:59 -0400

    Generique Room Finished
    Reviewer:
    Room:Generique Room
    DUN-86
    
    Functional doors and color changing doors with what is next.

--- Assets/Scripts/Managers/RoomManager.cs
+++ Assets/Scripts/Managers/RoomManager.cs
@@ -41,6 +41,10 @@ public class RoomManager
 
     }
 
+    public void RoomExited(Directions dir)
+    {
+        Debug.Log(dir);
+    }
     // // // 
 
 }
--- Assets/Scripts/Rooms/GeneriqueRooms.cs
+++ Assets/Scripts/Rooms/GeneriqueRooms.cs
@@ -21,18 +21,23 @@ public class GeneriqueRooms : MonoBehaviour
         { RoomType.Boss, new Color(1f,.84f,0f,1) },
         { RoomType.Enemy, new Color(.5f,.5f,.5f,1) },
         { RoomType.Shop, new Color(1f,0f,0f,1) },
-        { RoomType.Spawn, new Color(.5f,.5f,.5f,1) }
+        { RoomType.Spawn, new Color(.5f,.5f,.5f,1) },
+        { RoomType.None, new Color(.5f,.5f,.5f,0) }
     };
-    
+
     // Start is called before the first frame update
     public virtual void Initialize(int _lvl, RoomType[] _doors)
     {
-        
+        lvl = _lvl;   
         doors = _doors;
         north.SetActive(doors[0] == RoomType.None);
         east.SetActive(doors[1] == RoomType.None);
         south.SetActive(doors[2] == RoomType.None);
         west.SetActive(doors[3] == RoomType.None);
+        ColorDoor(doors[0], Directions.North);
+        ColorDoor(doors[1], Directions.East);
+        ColorDoor(doors[2], Directions.South);
+        ColorDoor(doors[3], Directions.West);
         //doors[1]  east
         //doors[2]  south
         //doors[3]  west
@@ -55,6 +60,7 @@ public class GeneriqueRooms : MonoBehaviour
         east.SetActive(true);
         south.SetActive(true);
         west.SetActive(true);
+        tileDoors.GetComponent<TilemapCollider2D>().enabled = false;
     }
 
     public virtual void UnlockDoors()
@@ -63,33 +69,87 @@ public class GeneriqueRooms : MonoBehaviour
         east.SetActive(false);
         south.SetActive(false);
         west.SetActive(false);
+        tileDoors.GetComponent<TilemapCollider2D>().enabled = true;
     }
 
     public void ColorDoor(RoomType door,Directions dir)
     {
         Tilemap tm = tileDoors;
         Color newDoorColor = doorColorDic[door];
+        Vector3Int pos0, pos1;
+        pos0 = pos1 = new Vector3Int();
 
         switch (dir)
         {
             case Directions.North:
-                tm.SetColor(new Vector3Int(11, 1, 0), newDoorColor);
-                tm.SetColor(new Vector3Int(12, 1, 0), newDoorColor);
+                pos0 = new Vector3Int(-1, 4, 0);
+                pos1 = new Vector3Int(0, 4, 0);
+                //tm.SetColor(new Vector3Int(11, 1, 0), newDoorColor);
+                //tm.SetColor(new Vector3Int(12, 1, 0), newDoorColor);
                 break;
             case Directions.South:
-                tm.SetColor(new Vector3Int(11, 10, 0), newDoorColor);
-                tm.SetColor(new Vector3Int(12, 10, 0), newDoorColor);
+                pos0 = new Vector3Int(-1, -4, 0);
+                pos1 = new Vector3Int(0, -4, 0);
+                //tm.SetColor(new Vector3Int(11, 10, 0), newDoorColor);
+                //tm.SetColor(new Vector3Int(12, 10, 0), newDoorColor);
                 break;
             case Directions.East:
-                tm.SetColor(new Vector3Int(21, 6, 0), newDoorColor);
-                tm.SetColor(new Vector3Int(21, 7, 0), newDoorColor);
+                pos0 = new Vector3Int(11, 0, 0);
+                pos1 = new Vector3Int(11, -1, 0);
+                //tm.SetColor(new Vector3Int(21, 6, 0), newDoorColor);
+                //tm.SetColor(new Vector3Int(21, 7, 0), newDoorColor);
                 break;
             case Directions.West:
-                tm.SetColor(new Vector3Int(0, 6, 0), newDoorColor);
-                tm.SetColor(new Vector3Int(0, 7, 0), newDoorColor);
+                pos0 = new Vector3Int(-11, 0, 0);
+                pos1 = new Vector3Int(-11, -1, 0);
+                //tm.SetColor(new Vector3Int(0, 6, 0), newDoorColor);
+                //tm.SetColor(new Vector3Int(0, 7, 0), newDoorColor);
                 break;
             default:
                 break;
         }
+        //newDoorColor.a = .1f;
+        //pos0 = new Vector3Int(-1,4,0);
+        //pos1 = new Vector3Int(0,4,0);
+        tm.SetTileFlags(pos0, TileFlags.None);
+        tm.SetTileFlags(pos1, TileFlags.None);
+        tm.SetColor(pos0, newDoorColor);
+        tm.SetColor(pos1, newDoorColor);
+
+
+        
+    }
+
+    public void DoorTriggerEntered(Transform thingEnteredDoor)
+    {
+        Debug.Log("thing entered door: " + thingEnteredDoor.name);
+        //once confirmed is player & location
+        if (thingEnteredDoor.CompareTag("Player"))
+        {
+            if(thingEnteredDoor.position.x <= 1 && thingEnteredDoor.position.x >= -1)
+            {
+                if(thingEnteredDoor.position.y >= 3)
+                {
+                    RoomManager.Instance.RoomExited(Directions.North);
+                }
+                else if(thingEnteredDoor.position.y <= -3)
+                {
+                    RoomManager.Instance.RoomExited(Directions.South);
+                }
+            }
+            else if (thingEnteredDoor.position.y <= 1 && thingEnteredDoor.position.y >= -1)
+            {
+                if (thingEnteredDoor.position.x >= 9)
+                {
+                    RoomManager.Instance.RoomExited(Directions.East);
+                }
+                else if (thingEnteredDoor.position.x <= -9)
+                {
+                    RoomManager.Instance.RoomExited(Directions.West);
+                }
+            }
+        }
+        //calculate which direction
+        //RoomManager.Instance.RoomExited(Directions.North)
     }
 }
