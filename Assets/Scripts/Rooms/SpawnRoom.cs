56d7f6c0be0620199bc42f19d9cdf5ba69d000d6
Philippe Bousquet <pbousque@isimtl.local>
2019-06-10 11:23:25 -0400

    Spawn Room
    Reviewer:Khalil
    Room:Spawn Room
    DUN-38
    
    Basic Player Spawn room which set the player position to 0.

--- Assets/Scripts/Rooms/SpawnRoom.cs
+++ Assets/Scripts/Rooms/SpawnRoom.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class SpawnRoom : GeneriqueRooms
 {
-  
+    public override void Initialize(int _lvl, RoomType[] _doors)
+    {
+        base.Initialize(_lvl, _doors);
+        PlayerManager.Instance.player.transform.position = new Vector3();
+    }
+
+    public override void RoomUpdate()
+    {
+        base.RoomUpdate();
+    }
+
+    public override void Close()
+    {
+        base.Close();
+    }
 }
