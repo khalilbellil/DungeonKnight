24e66dece74f576d207de195aaa4f98dc0ca4f9b
PreachTheScience <51325868+PreachTheScience@users.noreply.github.com>
2019-06-10 22:29:35 -0400

    Building Skeleton For StateMachine
    reviewer : Home
    Enemy
    DUN-40
    Description : creating base logic of the stateMachine, BaseState script, StateMachine Script, Transition script.

---
+++ Assets/Scripts/Ai/BaseState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseState
+{
+    public enum eUnitState{ATTACK,MOVE,DODGE}
+
+    public BaseState(BaseUnit unit,  List<Transition> list)
+    {
+
+    }
+
+    virtual public void Enter()
+    {
+        
+    }
+    
+    virtual public void Update()
+    {
+        
+    }
+
+    virtual public void Exit()
+    {
+
+    }
+
+    public eUnitState Evaluate()
+    {
+        return eUnitState.ATTACK;
+    }
+}
---
+++ Assets/Scripts/Ai/StateMachine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public enum eUnitState { ATTACK, MOVE, DODGE }
+
+    //Dictionary<eUnitState, BaseState> stateDict = new Dictionary<eUnitState, BaseState> { { eUnitState.ATTACK, new AttackState() }, { eUnitState.MOVE, new MoveState() }, { eUnitState.DODGE, new DodgeState() } };
+
+    public void Update()
+    {
+        //updateSenses();
+
+        //stateDict[CurrentState].update();
+        //eUnitState newState = stateDict[CurrentState].Evaluate();
+        /*if (newState != CurrentState)
+        {
+            stateDict[CurrentState].Exit();
+            CurrentState = newState;
+            stateDict[CurrentState].Enter();
+        }*/
+    }
+
+}
